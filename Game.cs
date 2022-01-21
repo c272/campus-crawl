82df69feb0053c55451869fa1650333d1ae7b42c
Anuj <annnujb@gmail.com>
2022-01-21 17:19:56 +0000

    Added base scenes and example assets

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tileEngine.SDK;
+using CampusCrawl.Scenes;
+
+namespace CampusCrawl
+{
+   public class Game : TileEngineGame
+    {
+        public override void Initialize()
+        {
+            TileEngine.Instance.SetScene(typeof(AlexandraSquare));
+        }
+
+        public override void Shutdown()
+        {
+            //...
+        }
+    }
+}
+
