12d69a61267e33cbaa1914911bb3311735e0beb8
michael0perdue <michael0perdue@gmail.com>
2022-01-23 18:19:52 +0000

    Removed unnecessary code

--- Game.cs
+++ Game.cs
@@ -13,38 +13,13 @@ namespace CampusCrawl
 {
    public class Game : TileEngineGame
    {
-        Character mainPlayer;
         public override void Initialize()
         {
-            mainPlayer = new Character();
+            var mainPlayer = new Character();
             TileEngine.Instance.SetScene(typeof(AlexandraSquare));
             TileEngine.Instance.GetScene().AddObject(mainPlayer);
-            mainPlayer.SetLayer("Objects");/*
-            TileEngine.Instance.KeyboardInput.AddBinding(Keys.W, "Up",false,false,moveUp);
-            TileEngine.Instance.KeyboardInput.AddBinding(Keys.S, "Down",false,false,moveDown);
-            TileEngine.Instance.KeyboardInput.AddBinding(Keys.A, "Left",false, false, moveLeft);
-            TileEngine.Instance.KeyboardInput.AddBinding(Keys.D, "Right",false, false, moveRight);
-            // TODO: TileEngine.Instance.GetScene().AddObject(mainPlayer);*/
-            // TODO: Latch onto scene change event to call that ^
+            mainPlayer.SetLayer("Objects");
         }
-
-        private void moveLeft()
-        {
-            mainPlayer.Position = mainPlayer.Position + new Vector2(-1, 0);
-        }
-        private void moveRight()
-        {
-            mainPlayer.Position = mainPlayer.Position + new Vector2(1, 0);
-        }
-        private void moveUp()
-        {
-            mainPlayer.Position = mainPlayer.Position + new Vector2(0, -1);
-        }
-        private void moveDown()
-        {
-            mainPlayer.Position = mainPlayer.Position + new Vector2(0, 1);
-        }
-
         public override void Shutdown()
         {
             //...
