b15c148072a184defd922b23244a7660fc71d427
michael0perdue <michael0perdue@gmail.com>
2022-01-23 18:17:54 +0000

    Added movement with WASD and collision works between player and a tile with collision

--- Characters/Character.cs
+++ Characters/Character.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using tileEngine.SDK;
 using tileEngine.SDK.Components;
+using tileEngine.SDK.Input;
 
 namespace CampusCrawl.Characters
 {
@@ -21,15 +23,20 @@ namespace CampusCrawl.Characters
             });
             Components.Add(new SpriteComponent
             {
-                Texture = AssetManager.AttemptLoad<Texture2D>(-1280955819),
+                Texture = AssetManager.AttemptLoad<Texture2D>(1215427970),
                 Position = new Vector2(0, 0),
                 Scale = new Vector2(1,1)
-            }); 
+            });
+            TileEngine.Instance.KeyboardInput.AddAxisBinding(Keys.D, Keys.A, Keys.S, Keys.W, "Movement");
+
         }
 
         public override void Update(GameTime delta)
         {
             base.Update(delta);
+            var movement = InputHandler.GetEvent("Movement");
+            this.Position = new Vector2(Position.X + movement.Value.X , Position.Y + movement.Value.Y );
+            
             //...
         }
     }
--- Game.cs
+++ Game.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +12,37 @@ using CampusCrawl.Characters;
 namespace CampusCrawl
 {
    public class Game : TileEngineGame
-    {
+   {
+        Character mainPlayer;
         public override void Initialize()
         {
-            var mainPlayer = new Character();
+            mainPlayer = new Character();
             TileEngine.Instance.SetScene(typeof(AlexandraSquare));
             TileEngine.Instance.GetScene().AddObject(mainPlayer);
-            mainPlayer.SetLayer("Objects");
-            // TODO: TileEngine.Instance.GetScene().AddObject(mainPlayer);
+            mainPlayer.SetLayer("Objects");/*
+            TileEngine.Instance.KeyboardInput.AddBinding(Keys.W, "Up",false,false,moveUp);
+            TileEngine.Instance.KeyboardInput.AddBinding(Keys.S, "Down",false,false,moveDown);
+            TileEngine.Instance.KeyboardInput.AddBinding(Keys.A, "Left",false, false, moveLeft);
+            TileEngine.Instance.KeyboardInput.AddBinding(Keys.D, "Right",false, false, moveRight);
+            // TODO: TileEngine.Instance.GetScene().AddObject(mainPlayer);*/
             // TODO: Latch onto scene change event to call that ^
+        }
+
+        private void moveLeft()
+        {
+            mainPlayer.Position = mainPlayer.Position + new Vector2(-1, 0);
+        }
+        private void moveRight()
+        {
+            mainPlayer.Position = mainPlayer.Position + new Vector2(1, 0);
+        }
+        private void moveUp()
+        {
+            mainPlayer.Position = mainPlayer.Position + new Vector2(0, -1);
+        }
+        private void moveDown()
+        {
+            mainPlayer.Position = mainPlayer.Position + new Vector2(0, 1);
         }
 
         public override void Shutdown()
