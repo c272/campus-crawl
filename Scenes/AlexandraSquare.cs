a38769045ad66bcd05f5ffd06438904662968898
michael0perdue <michael0perdue@gmail.com>
2022-02-16 20:05:41 +0000

    added a background bird sound

--- Scenes/AlexandraSquare.cs
+++ Scenes/AlexandraSquare.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tileEngine.SDK;
-
+using tileEngine.SDK.Audio;
 
 namespace CampusCrawl.Scenes
 {
@@ -14,6 +14,10 @@ namespace CampusCrawl.Scenes
         public override void Initialize()
         {
             base.Initialize();
+            SoundReference loadedSound = TileEngine.Instance.Sound.LoadSound("Sound/birdSound.mp3");
+            SoundInstance soundInstance = TileEngine.Instance.Sound.PlaySound(loadedSound);
+            soundInstance.Looping = true;
+            soundInstance.Volume = 0.3f;
             //...
         }
 
